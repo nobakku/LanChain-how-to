7a428c9523f1e1e5169dedfd099af204ebc4d3a8
nobata <ahirururu122@gmail.com>
2023-08-10 15:44:04 +0900

    ユーザー入力欄の修正

--- chatGPT_clone_app.py
+++ chatGPT_clone_app.py
@@ -1,14 +1,31 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain.schema import (
+    SystemMessage,  # システムメッセージ(AIの"キャラ設定")
+    HumanMessage,  # 人間の質問
+    AIMessage  # ChatGPTの返答
+)
+
+import os
+from dotenv import load_dotenv
+
+# 環境変数の読み込みと取り出し
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
 
 
 
 def main():
-    llm = ChatOpenAI(temperature=0)
+    llm = ChatOpenAI(temperature=0) # ChatGPT APIを呼んでくれる機能
 
-    st.set_page_config(page_title="My Great ChatGPT", page_icon="🤗")
+    st.set_page_config(
+        page_title="My Great ChatGPT",
+        page_icon="🤗"
+    )
     st.header("My Great ChatGPT 🤗")
+
 
     # チャット履歴の初期化
     if "messages" not in st.session_state:
@@ -17,24 +34,32 @@ def main():
         ]
 
     # ユーザーの入力を監視
-    if user_input := st.chat_input("聞きたいことを入力してね！"):
+    container = st.container()
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_area(label='Message: ', key='input', height=100)
+            submit_button = st.form_submit_button(label='送信')
+
+    if submit_button and user_input:
+        # 何か入力されて Submit ボタンが押されたら実行される
         st.session_state.messages.append(HumanMessage(content=user_input))
         with st.spinner("ChatGPT is typing ..."):
             response = llm(st.session_state.messages)
         st.session_state.messages.append(AIMessage(content=response.content))
 
+
     # チャット履歴の表示
-    messages = st.session_state.get("messages", [])
+    messages = st.session_state.get('messages', [])
     for message in messages:
         if isinstance(message, AIMessage):
-            with st.chat_message("assistant"):
+            with st.chat_message('assistant'):
                 st.markdown(message.content)
         elif isinstance(message, HumanMessage):
-            with st.chat_message("user"):
+            with st.chat_message('user'):
                 st.markdown(message.content)
         else:  # isinstance(message, SystemMessage):
             st.write(f"System message: {message.content}")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
